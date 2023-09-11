3bfa95202873f2ce0de9cb60d48a19b64a5bfb28
Vierkant#9976 <Stevebudding123@gmail.com>
2023-09-11 11:43:04 +0200

    Project opdracht

---
+++ main.py
@@ -0,0 +1,10 @@
+# Een reiziger moet namelijk de mogelijkheid krijgen om een naam en bericht in te voeren.
+# Realiseer deze functionaliteit en print de ingevoerde gegevens ter bevestiging uit!
+
+naam = input("Naam: ")
+
+bericht = input("Bericht: ")
+
+print(f"Naam: {naam}")
+print(f"Bericht: {bericht}")
+
