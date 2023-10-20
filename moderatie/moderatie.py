6c9c4e3888c7745f698ecc5538e831e7d467cbf6
vierkant <Stevebudding123@gmail.com>
2023-10-20 15:15:19 +0200

    login scherm

--- moderatie/moderatie.py
+++ moderatie/moderatie.py
@@ -1,4 +1,5 @@
 import psycopg2
+import psycopg2.extras # DictCursor
 import csv
 from tkinter import * 
 
@@ -14,8 +15,44 @@ class Modereer():
         self.bestand = berichten_bestand_naam
         self.index = 0 # hoe ver we zijn met beoordelen
 
+        self.con = None
+        self.cursor = None
+
         with open(self.bestand) as f:
             self.berichten = list(csv.DictReader(f))
+
+    def connect(self):
+        if self.con != None:
+            print("Er is al een connectie gemaakt.")
+            return
+        
+        self.con = psycopg2.connect(
+                    database="StationZuil", 
+                    user="postgres",
+                    password="pass",
+                    host="localhost",
+                    port="5433"
+                    )
+        
+        self.cursor = self.con.cursor()
+
+        print(self.con.status)
+
+    def disconnect(self):
+        if self.con.closed:
+            return
+        
+        print("connectie sluiten")
+        self.con.close()
+
+    def login(self, email, wachtwoord):
+        self.cursor.execute("""
+                            select count(*) from moderator 
+                            where email = %s and wachtwoord = %s
+                            """,
+                            (email, wachtwoord))
+        
+        return self.cursor.fetchone()[0] == 1
         
     def geef_bericht(self):
         """Geeft berichten terug die nog niet zijn beoordeeld"""
@@ -26,8 +63,7 @@ class Modereer():
     def beoordeel_bericht(self, goedgekeurd):
         """Beoordeel bericht op index"""
         if goedgekeurd:
-            # stuur naar database
-            pass
+            self.bericht[self.index]
 
         self.index += 1
         
@@ -42,61 +78,83 @@ class Modereer():
             f.write(lines[0])
             if self.index != len(self.berichten):
                 f.writelines(lines[self.index + 1:])
-                print(self.index, len(self.berichten))
 
             self.index = 0
+
         with open(self.bestand, 'r') as f:
             self.berichten = list(csv.DictReader(f))
 
-# TODO: update csv bij aflsuiten
 class ModeratieGUI():
     def __init__(self, naam_window: str = "moderatie"):
         self.moderatie = Modereer("berichten.csv")
+
+        self.moderatie.connect()
 
         self.root = Tk()
         self.root.title(naam_window)
         self.root.resizable(False, False)
         self.root.geometry("500x500")
 
+        # moderatie scherm
         self.info_frame = LabelFrame(self.root, height=300, width=450, text="bericht")
-        self.info_frame.pack_propagate(False)
-        self.info_frame.pack(padx=20, pady=10)
-
-        # self.info_bericht_frame = Frame(self.info_frame, height=100, width=200)
-        # self.info_bericht_frame.pack(padx=20, pady=10, side=BOTTOM)
-
-        # laat voorgang van beoordelen zien
-        # self.info_label_counter = Label(self.info_frame, text="")
-        # self.info_label_counter.pack()
-
-        # bericht
         self.naam = Label(self.info_frame)
         self.station = Label(self.info_frame)
         self.tijd = Label(self.info_frame)
         self.bericht = Text(self.info_frame, height=10)
-
-        self.update_bericht_labels()
-
         self.bericht["state"] = DISABLED
+
+        # moderatie 
+        self.moderatie_frame = LabelFrame(self.root, height=200, width=450, text="moderatie")
+        self.btn_goedkeuren = Button(self.moderatie_frame, text="goedkeuren", command=self.on_goedkeuring)
+        self.btn_afkeuren = Button(self.moderatie_frame, text="afkeuren", command=self.on_afkeuring)
+        self.btn_update = Button(self.moderatie_frame, text="update csv", command=self.on_update_csv)
+
+        # login scherm 
+        self.login_frame = LabelFrame(self.root, text="login", height=400, width=300)
+        self.login_email = Entry(self.login_frame)
+        self.login_wachtwoord = Entry(self.login_frame)
+        self.btn_login = Button(self.login_frame, text="login", command=self.on_login)
+        self.foutmelding_label = Label(self.login_frame, foreground=RED)
+
+        self.toon_login()
+        
+    def toon_login(self):
+        # verberg moderatie scherm
+        self.info_frame.pack_forget()
+
+        self.login_frame.pack_propagate(False)
+        self.login_frame.pack()
+
+        # widgets
+        self.login_email.pack()
+        self.login_wachtwoord.pack()
+        self.btn_login.pack()
+        self.foutmelding_label.pack()
+
+    def toon_moderatie(self):
+        # verberg login scherm 
+        self.login_frame.pack_forget()
+
+        self.info_frame.pack_propagate(False)
+        self.info_frame.pack(padx=20, pady=10)
         self.bericht.pack(padx=20, pady=10, side=BOTTOM, anchor=CENTER)
         self.naam.pack(padx=50, pady=2, side=TOP, anchor=CENTER)
         self.station.pack(padx=50, pady=2, side=TOP, anchor=CENTER)
         self.tijd.pack(padx=50, pady=2, side=TOP, anchor=CENTER)
 
-        # moderatie 
-        self.optie_frame = LabelFrame(self.root, height=200, width=450, text="moderatie")
-        self.optie_frame.pack_propagate(False)
-        self.optie_frame.pack(pady=10)
-    
-        self.btn_goedkeuren = Button(self.optie_frame, text="goedkeuren", command=self.on_goedkeuring)
+        self.moderatie_frame.pack_propagate(False)
+        self.moderatie_frame.pack(pady=10)
         self.btn_goedkeuren.pack(padx=10, anchor=CENTER)
-
-        self.btn_afkeuren = Button(self.optie_frame, text="afkeuren", command=self.on_afkeuring)
         self.btn_afkeuren.pack(padx=10, anchor=CENTER)
-
-        self.btn_update = Button(self.optie_frame, text="update csv", command=self.update_csv)
         self.btn_update.pack(padx=10, anchor=CENTER)
-        
+    
+    def on_login(self):
+        if self.moderatie.login(self.login_email.get(), self.login_wachtwoord.get()):
+            self.toon_moderatie()
+            self.update_bericht_labels()
+        else:
+            self.foutmelding_label["text"] = "Fout wachtwoord of email adress."
+
     def on_goedkeuring(self):
         self.moderatie.beoordeel_bericht(True)
         self.update_bericht_labels()
@@ -134,32 +192,19 @@ class ModeratieGUI():
         self.station["text"] = f"station: {station}" 
         self.tijd["text"] = f"tijd: {tijd}" 
 
-    def update_csv(self):
+    def on_update_csv(self):
         self.moderatie.update_bestand()
 
     def show(self):
         self.root.mainloop()
 
+        self.on_update_csv()
+        self.moderatie.disconnect()
+
 
 def main():
     GUI = ModeratieGUI()
     GUI.show()
-    # con = psycopg2.connect(
-    #                 database="StationZuil", 
-    #                 user="postgres",
-    #                 password="pass",
-    #                 host="localhost",
-    #                 port="5433"
-    #                 )
-
-    # cursor = con.cursor()
-
-    # cursor.execute("select * from station_service;")
-    # print(cursor.fetchall())
-
-    # if not con.closed:
-    #     con.close()
-
 
 if __name__ == "__main__":
     main()
