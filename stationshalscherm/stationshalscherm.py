8e8da662982aa80b271fc541bc02f5997ab8d209
vierkant <Stevebudding123@gmail.com>
2023-10-25 09:47:10 +0200

    module 3 bestanden toegevoegd

---
+++ stationshalscherm/stationshalscherm.py
@@ -0,0 +1,6 @@
+import requests # open weather api 
+
+# laat de laatste 5 berichten zien (de meest recente) op chronologische volgorde 
+# voor elk bericht pak het station en daarvan de faciliteiten (station_service table)
+# voor het station zelf waar het scherm staat, laat de weersvoorspelling zien https://openweathermap.org/
+
