5f25e61a2421e259041279e8fe516bbb1be1967e
vierkant <Stevebudding123@gmail.com>
2023-11-03 23:07:05 +0100

    totaal overzicht window WIP

--- opdracht/moderatie/moderatieGUI.py
+++ opdracht/moderatie/moderatieGUI.py
@@ -1,5 +1,5 @@
 from modereer import *
-from tkinter import * 
+from tkinter import *
 from tkinter import messagebox
 
 GREEN = "#0F0"
@@ -9,23 +9,125 @@ WHITE = "#FFF"
 GRAY = "#999"
 
 
+class BerichtPreviewWidget(LabelFrame):
+    def __init__(self, root, bericht, naam, station, datum, goedgekeurd, email):
+        super().__init__(root, text="")
+
+        self.bericht = bericht
+        self.naam = naam
+        self.station = station
+        self.datum = datum
+        self.goedgekeurd = goedgekeurd
+        self.email = email
+
+        # TODO: verklein te grote berichten
+
+        self.bericht_label = Label(self, text=bericht)
+        self.naam_label = Label(self, text=naam)
+        if goedgekeurd:
+            self.goedgekeurd_label = Label(self, text="Goedgekeurd", fg=GREEN)
+        else:
+            self.goedgekeurd_label = Label(self, text="Afgekeurd", fg=RED)
+
+        self.naam_label.pack(side=TOP)
+        self.bericht_label.pack()
+        self.goedgekeurd_label.pack()
+
+
+class TotaalOverzichtGUI:
+    """
+    Laat een totaal overzicht zien van alle beoordeelde berichten,
+    er kan als nodig wijzigingen worden gebracht aan de berichten
+    """
+
+    def __init__(self):
+        self.db_berichten_group_frames = None
+
+        self.db = database.StationsZuilDB()
+        connectie_resultaat = self.db.connect()
+
+        while connectie_resultaat == False:
+            try_again = messagebox.askretrycancel(
+                "Database",
+                f"Er kon geen connectie worden gemaakt met de database: {self.db.error_str}")
+            if try_again:
+                connectie_resultaat = self.db.connect()
+            else:
+                self.root = None
+                return
+
+        db_berichten = self.db.get_rows("""
+        select bericht.tekst, bericht.naam, bericht.station, bericht.datum, beoordeling.is_goedgekeurd, beoordeling.moderator_email from bericht
+        inner join beoordeling on bericht.beoordelingnr = beoordeling.beoordelingnr
+        """)
+
+        self.root = Tk()
+        self.root.title("Totaal Overzicht")
+        self.root.resizable(False, False)
+        self.root.geometry("500x600")
+
+        self.db_berichten_frame = Frame(self.root)
+        self.db_bericht_opties_frame = LabelFrame(self.root, text="bericht opties")
+
+        # layout is in rijen met elk 3 berichten (Frame met 3 Frames)
+        self.db_bericht_group_frame = []
+        cur_frame = None
+        for i, bericht in enumerate(db_berichten):
+            if i % 4 == 0:
+                if cur_frame != None:
+                    cur_frame.pack(side=TOP)
+
+                cur_frame = Frame(self.db_berichten_frame)
+                self.db_bericht_group_frame.append(cur_frame)
+                continue
+
+            bericht_frame = BerichtPreviewWidget(cur_frame,
+                                                 bericht[0],
+                                                 bericht[1],
+                                                 bericht[2],
+                                                 bericht[3],
+                                                 bericht[4],
+                                                 bericht[5])
+            bericht_frame.pack(side=LEFT)
+
+        self.btn_wijzig = Button(self.db_bericht_opties_frame, text="wijzig", command=self.on_wijzig)
+
+        # layout
+        self.db_berichten_frame.pack()
+        self.db_bericht_opties_frame.pack()
+
+    def on_wijzig(self):
+        pass
+
+    def show(self):
+        if self.root == None:
+            return
+
+        self.root.mainloop()
+
+
+g = TotaalOverzichtGUI()
+g.show()
+
+
 class ModeratieGUI:
     """Moderatie window en UI voor beoordelen van berichten"""
+
     def __init__(self, naam_window: str = "moderatie"):
         self.moderatie = Modereer("../../berichten.csv")
 
         connectie_resultaat = self.moderatie.connect()
 
-        while connectie_resultaat == False: 
+        while connectie_resultaat == False:
             try_again = messagebox.askretrycancel(
                 "Database",
                 f"Er kon geen connectie worden gemaakt met de database: {self.moderatie.error_str}")
-            if try_again: 
+            if try_again:
                 connectie_resultaat = self.moderatie.connect()
-            else: 
+            else:
                 self.root = None
                 return
-            
+
         # window
         self.root = Tk()
         self.root.title(naam_window)
@@ -86,7 +188,7 @@ class ModeratieGUI:
         self.btn_goedkeuren.pack(padx=10, anchor=CENTER)
         self.btn_afkeuren.pack(padx=10, anchor=CENTER)
         self.btn_update.pack(padx=10, anchor=CENTER)
-    
+
     def on_login(self):
         """Login knop press, probeer in te loggen"""
         if self.moderatie.login(self.login_email.get(), self.login_wachtwoord.get()):
@@ -110,7 +212,7 @@ class ModeratieGUI:
     def update_bericht_labels(self):
         """Update csv bestand knop press"""
         bericht_info = self.moderatie.geef_bericht()
-        
+
         # zodat we de widget kunnen aanpassen
         self.bericht["state"] = NORMAL
 
@@ -123,7 +225,7 @@ class ModeratieGUI:
             self.bericht.insert(END, bericht_info)
             self.bericht["state"] = DISABLED
             return
-        
+
         # update widgets met het nieuwe bericht
         naam = bericht_info["naam"]
         tijd = bericht_info["tijd"]
@@ -134,16 +236,16 @@ class ModeratieGUI:
         self.bericht.insert(END, bericht)
         self.bericht["state"] = DISABLED
 
-        self.naam["text"] = f"naam: {naam}" 
-        self.station["text"] = f"station: {station}" 
-        self.tijd["text"] = f"tijd: {tijd}" 
+        self.naam["text"] = f"naam: {naam}"
+        self.station["text"] = f"station: {station}"
+        self.tijd["text"] = f"tijd: {tijd}"
 
     def on_update_csv(self):
         self.moderatie.update_bestand()
 
     def show(self):
         # check of de database connectie fout ging
-        if self.root == None: 
+        if self.root == None:
             return
 
         self.root.mainloop()
