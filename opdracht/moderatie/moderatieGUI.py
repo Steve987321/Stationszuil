ed85a3802819a3c6e899f672390017bf442ec7dd
vierkant <Stevebudding123@gmail.com>
2023-11-06 10:37:11 +0100

    totaaloverzichtGUI verplaatst naar eigen bestand

--- opdracht/moderatie/moderatieGUI.py
+++ opdracht/moderatie/moderatieGUI.py
@@ -1,248 +1,14 @@
 from modereer import *
 from tkinter import *
 from tkinter import messagebox
+from totaaloverzichtscherm import TotaalOverzichtGUI
+
 
 GREEN = "#0F0"
 RED = "#F00"
 BLUE = "#00F"
 WHITE = "#FFF"
 GRAY = "#999"
-
-
-def clamp(n, a, b):
-    """klem n tussen a en b"""
-    if n > b:
-        return b
-    elif n < a:
-        return a
-    else:
-        return n
-
-
-class BerichtPreviewWidget(LabelFrame):
-    """Bericht widget frame
-    Laat belangrijke info zien van het bericht, kan ook alles tonen door op inspecteer te drukken
-    """
-    is_inspecteren = False
-
-    def __init__(self, root: Misc, group, bericht, naam, station, datum, tijd, goedgekeurd, email):
-        super().__init__(group, text="", width=150, height=100)
-
-        self.root = root
-
-        # preview
-
-        # verklein als nodig het bericht zodat het past in de widget
-        # verkort tot \n bij < 20
-        posnline = bericht.find('\n')
-        if posnline != -1 and posnline <= 20:
-            bericht_verkort = bericht[0:clamp(posnline, 0, 15)] + ".."
-            self.bericht_label = Label(self, text=bericht_verkort)
-        # geen \n maar bericht is langer dan 20
-        elif len(bericht) > 20:
-            bericht_verkort = bericht[0:15] + ".."
-            self.bericht_label = Label(self, text=bericht_verkort)
-        # bericht is al klein genoeg
-        else:
-            self.bericht_label = Label(self, text=bericht)
-
-        if goedgekeurd:
-            self.goedgekeurd_label = Label(self, text="Goedgekeurd", fg=GREEN)
-        else:
-            self.goedgekeurd_label = Label(self, text="Afgekeurd", fg=RED)
-
-        self.naam_label = Label(self, text=naam)
-
-        self.btn_inspecteer = Button(self, text="inspecteer", command=self.on_inspecteer)
-
-        self.naam_label.pack()
-        self.bericht_label.pack()
-        self.goedgekeurd_label.pack()
-        self.btn_inspecteer.pack()
-
-        # inspecteer bericht frame
-        self.full_preview_frame = LabelFrame(root, text="inspecteer bericht", width=300, height=300)
-        self.full_preview_frame.pack_propagate(False)
-
-        self.btn_sluit = Button(self.full_preview_frame, text="X", command=self.on_sluit_inspecteer)
-        self.naam_label_full = Label(self.full_preview_frame, text=f"naam: {naam}")
-        self.station_label_full = Label(self.full_preview_frame, text=f"station: {station}")
-        self.tijd_label_full = Label(self.full_preview_frame, text=f"datum: {datum}")
-        self.datum_label_full = Label(self.full_preview_frame, text=f"tijd: {tijd}")
-        self.datum_label_full = Label(self.full_preview_frame, text=f"beoordeeld door: {email}")
-        self.bericht_full = Text(self.full_preview_frame)
-        self.bericht_full.insert(END, bericht)
-        self.bericht_full["state"] = DISABLED
-
-        self.btn_sluit.pack()
-        self.naam_label_full.pack()
-        self.station_label_full.pack()
-        self.tijd_label_full.pack()
-        self.datum_label_full.pack()
-        self.bericht_full.pack()
-
-    def on_inspecteer(self):
-        """Laat inspecteer frame zien, bovenop andere alle preview frames"""
-        if BerichtPreviewWidget.is_inspecteren:
-            return
-
-        for widget in self.root.winfo_children():
-            try:
-                widget["state"] = DISABLED
-            except:  # kan niet worden disabled, dit kan worden negeert
-                pass
-
-        BerichtPreviewWidget.is_inspecteren = True
-
-        self.full_preview_frame.place(anchor=CENTER, relx=0.5, rely=0.5)
-
-    def on_sluit_inspecteer(self):
-        """Sluit het inspecteer frame"""
-        BerichtPreviewWidget.is_inspecteren = False
-        self.full_preview_frame.place_forget()
-
-
-class TotaalOverzichtGUI:
-    """Laat een totaal overzicht zien van alle beoordeelde berichten"""
-    def __init__(self, db: database.StationsZuilDB = None):
-        self.paginas = []
-
-        # layout is in rijen met elk 3 berichten (Frame met 3 Frames)
-        # max 5 rijen in 600 hoogte
-        self.db_bericht_group_frame = []
-
-        # geselecteerde pagina
-        self.pagina = 0
-
-        # database connectie
-
-        if db == None:
-            self.db = database.StationsZuilDB()
-            connectie_resultaat = self.db.connect()
-
-            while connectie_resultaat == False:
-                try_again = messagebox.askretrycancel(
-                    "Database",
-                    f"Er kon geen connectie worden gemaakt met de database: {self.db.error_str}")
-                if try_again:
-                    connectie_resultaat = self.db.connect()
-                else:
-                    self.root = None
-                    return
-        else:
-            self.db = db
-
-        db_berichten = self.db.get_rows("""
-        select bericht.tekst, bericht.naam, bericht.station, bericht.datum, bericht.tijd, beoordeling.is_goedgekeurd, beoordeling.moderator_email from bericht
-        inner join beoordeling on bericht.beoordelingnr = beoordeling.beoordelingnr
-        """)
-
-        pagina_index = 0
-
-        # groepeer de berichten in groepen van 15
-        if len(db_berichten) > 15:
-            for i in range(15, len(db_berichten), 15):
-                self.paginas.append(db_berichten[:i])
-                db_berichten = db_berichten[i:]
-                pagina_index += 1
-
-        if len(db_berichten) >= 0:
-            self.paginas.append([])
-            for bericht in db_berichten:
-                self.paginas[pagina_index].append(bericht)
-
-        self.root = Toplevel()
-        self.root.title("Totaal Overzicht")
-        self.root.resizable(False, False)
-        self.root.geometry("500x600")
-
-        navigatie_frame = Frame(self.root)
-
-        self.pagina_label = Label(navigatie_frame, text=f"{self.pagina + 1}/{len(self.paginas)}")
-        self.btn_pagina_rechts = Button(navigatie_frame, text=">", command=self.pagina_rechts)
-        self.btn_pagina_links = Button(navigatie_frame, text="<", command=self.pagina_links)
-        self.db_berichten_frame = Frame(self.root)
-
-        self.toon_pagina(0)
-
-        # layout
-        self.db_berichten_frame.pack()
-
-        navigatie_frame.pack(side=BOTTOM)
-        self.btn_pagina_links.pack(side=LEFT)
-        self.btn_pagina_links["state"] = DISABLED
-        self.pagina_label.pack(side=LEFT)
-        self.btn_pagina_rechts.pack(side=LEFT)
-
-    def is_open(self):
-        return self.root.winfo_exists()
-
-    def pagina_rechts(self):
-        """Ga naar volgende pagina"""
-        self.btn_pagina_links["state"] = NORMAL
-        self.btn_pagina_rechts["state"] = NORMAL
-
-        self.pagina += 1
-        self.toon_pagina(self.pagina)
-        self.pagina_label["text"] = f"{self.pagina + 1}/{len(self.paginas)}"
-
-        if self.pagina == len(self.paginas) - 1:
-            self.btn_pagina_rechts["state"] = DISABLED
-
-    def pagina_links(self):
-        """Ga naar vorige pagina"""
-        self.btn_pagina_rechts["state"] = NORMAL
-        self.btn_pagina_links["state"] = NORMAL
-
-        self.pagina -= 1
-        self.toon_pagina(self.pagina)
-        self.pagina_label["text"] = f"{self.pagina + 1}/{len(self.paginas)}"
-
-        if self.pagina == 0:
-            self.btn_pagina_links["state"] = DISABLED
-
-    def toon_pagina(self, pagina_index):
-        """Toont de pagina met berichten
-
-        Args:
-            pagina_index: index van pagina vanaf 0
-        """
-        cur_frame = None
-
-        # verwijder vorige
-        for frame in self.db_bericht_group_frame:
-            for child_frame in frame.winfo_children():
-                child_frame.pack_forget()
-                child_frame.destroy()
-            frame.pack_forget()
-            frame.destroy()
-
-        self.db_bericht_group_frame.clear()
-
-        for frame in self.db_berichten_frame.winfo_children():
-            frame.pack_forget()
-            frame.destroy()
-
-        for i, bericht in enumerate(self.paginas[pagina_index]):
-            if i % 3 == 0:
-                cur_frame = Frame(self.db_berichten_frame)
-                self.db_bericht_group_frame.append(cur_frame)
-
-            bericht_frame = BerichtPreviewWidget(self.root,
-                                                 cur_frame,
-                                                 bericht=bericht[0],
-                                                 naam=bericht[1],
-                                                 station=bericht[2],
-                                                 datum=bericht[3],
-                                                 tijd=bericht[4],
-                                                 goedgekeurd=bericht[5],
-                                                 email=bericht[6])
-
-            bericht_frame.pack_propagate(False)
-            bericht_frame.pack(side=LEFT, padx=2, pady=5)
-
-        for frame in self.db_bericht_group_frame:
-            frame.pack(side=TOP)
 
 
 class ModeratieGUI:
@@ -375,6 +141,11 @@ class ModeratieGUI:
             self.bericht.delete("1.0", END)
             self.bericht.insert(END, bericht_info)
             self.bericht["state"] = DISABLED
+
+            # zet beoordeel knoppen uit als er geen berichten zijn om te beoordelen
+            self.btn_afkeuren["state"] = DISABLED
+            self.btn_goedkeuren["state"] = DISABLED
+
             return
 
         # update widgets met het nieuwe bericht
@@ -390,6 +161,10 @@ class ModeratieGUI:
         self.naam["text"] = f"naam: {naam}"
         self.station["text"] = f"station: {station}"
         self.tijd["text"] = f"tijd: {tijd}"
+
+        # zet beoordeel knoppen uit als er geen berichten zijn om te beoordelen
+        self.btn_afkeuren["state"] = NORMAL
+        self.btn_goedkeuren["state"] = NORMAL
 
     def on_update_csv(self):
         self.moderatie.update_bestand()
